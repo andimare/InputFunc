983740fd0027573f50fc0362d666220f32317763
andi mare <andrewo.mare72@gmail.com>
2024-02-23 15:14:16 +0300

    Add the input version that would handle double data type  input

--- inputFunc.cpp
+++ inputFunc.cpp
@@ -26,15 +26,31 @@ int inputi(string Sentence){
 	return num;
 }
 
+double inputd(string Sentence){
+	stringstream giik;
+	string var;
+	double num;
+	
+	cout<<Sentence<<endl;
+	cin>>var;
+	
+	giik<<var;
+	giik>>num;
+	
+	return num;
+}
+
 int main(){
 	string name;
 	int age = 0;
 	
 	name = inputstr("Enter your name: ");
 	age = inputi("Enter your age: ");
+	double ff = inputd("Enter a Decimal number: ");
 	
 	cout<<"Welcome here Mr. "<<name<<endl;
 	cout<<"Next year you will be "<<age+1<<" years old."<<endl;
+	cout<<"This is you float number "<<ff + 0.103<<endl;
 	
 	return 0;
 }
